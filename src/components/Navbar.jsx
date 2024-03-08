e04d646fd9f47342a31be86bce184ef58751393a
DevJaimeSantiago <jaimestgo475@gmail.com>
2024-03-08 14:36:25 -0600

    changes in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ export function Navbar() {
   ]
 
   return (
-    <div className='flex justify-between items-center w-full h-20 fixed text-white bg-black px-4'>
+    <div className='flex justify-between items-center w-full h-20 fixed text-white bg-gradient-to-r from-[#ed00ff] to-fuchsia-700 px-4'>
       <div>
         <h1 className='text-5xl font-signature ml-2'>DevJaimeSantiago</h1>
       </div>
@@ -35,7 +35,7 @@ export function Navbar() {
         {Links.map(({ id, link }) => (
           <li
             key={id}
-            className='px-4 cursor-pointer capitalize font-medium text-gray-300 hover:scale-105 duration-200'>
+            className='px-4 cursor-pointer capitalize font-medium text-gray-100 hover:scale-105 duration-200 text-xl'>
             {link}
           </li>
         ))}
@@ -43,12 +43,12 @@ export function Navbar() {
 
       <div
         onClick={handleNav}
-        className='cursor-pointer pr-4 z-10 text-gray-300 md:hidden '>
+        className='cursor-pointer pr-4 z-10 text-gray-100 md:hidden '>
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-300 md:hidden'>
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-fuchsia-700 to-[#ed00ff] text-gray-100 md:hidden'>
           {Links.map(({ id, link }) => (
             <li
               key={id}
